5a4194bb9ae38c121a367b8a6948a3cc0de37798
Aaron Ridley <ridley@umich.edu>
2022-04-17 10:37:08 -0400

    FEAT: hardcoded some things. Trying to get dwell spot to work. Doesn't yet.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-#!/Users/ridley/bin/Enthought/User/bin/python
-#/opt/local/bin/python
+#!/usr/bin/env python
 
 # Ronnie Frank Pires Stone 
 # Satellite Earth Imaging Project 
@@ -16,7 +15,7 @@ import glob # Python's unix pathname expansion library
 import os # Python's operating system functionality library 
 import matplotlib.pyplot as plt	# Python's plotting library 
 from PIL import Image  # Python's image processing library 
-from scipy.misc import toimage # Python's scientific library 
+#from scipy.misc import toimage # Python's scientific library 
 
 # Custom Functions for the Application:
 
@@ -37,6 +36,18 @@ from area_lonlat import area_lonlat
 from missing_lonlat import missing_lonlat
 
 from iono_routines import *
+
+
+def make_vector(x, y, z):
+    v = [x, y, z]
+    m = np.sqrt(x*x + y*y + z*z)
+    v = v / m
+    return v
+
+def get_angle(v1, v2):
+    dot = np.sum(v1 * v2)
+    angle = np.arccos(dot)
+    return angle
 
 # ------------------------------------------------------------------
 # 1) Data fetching 
@@ -65,22 +76,30 @@ print(dt)
 
 # Input data:
 
-n = int(raw_input('Horizontal Number of Pixels? '))
-m = int(raw_input('Vertical Number of Pixels? '))
-nTimes = int(raw_input('Number of frames? '))
-nSats = int(raw_input('Number of Satellites? '))
+#n = int(raw_input('Horizontal Number of Pixels? '))
+#m = int(raw_input('Vertical Number of Pixels? '))
+#nTimes = int(raw_input('Number of frames? '))
+#nSats = int(raw_input('Number of Satellites? '))
+
+n = 128*2
+m = 128*2
+nTimes = 3
+nSats = 2
 
 dtor = np.pi / 180.0
 
 rEarth = 6372.0 # Approximated and given in kilometers (Earth Radius)
-rOrbit = rEarth * 3.5
+rOrbit = rEarth + 20000.0
 
 alpha = 18.0 * dtor # Approximated and given in radians (Field of View)
 
 # Satellite(s) coordinates in time (3D Cartesian System centered at the Earth)
-print(nSats, rOrbit, nTimes, dt)
+
 OrbitX, OrbitZ = orbital_planner(nSats, rOrbit, nTimes, dt)
  
+print(OrbitX)
+print(OrbitZ)
+
 # ------------------------------------------------------------------
 # 2) Reference image for method implementation processing 
 # -----------------------------------------------------------------------
@@ -97,13 +116,14 @@ z = rOrbit
 # Produce reference image: 
 
 # Equatorial grid 
-(NM_lon, NM_lat) = image_grid(x,y,z,n,m,alpha,rEarth)
+(NM_lon, NM_lat) = image_grid(x,y,z,n,m,alpha,rEarth, 0.0)
 # Rotates to north pole 
 (NM_lon_rotated, NM_lat_rotated) = image_rotation(NM_lon, NM_lat, x, y, z, rEarth)
 # Produces the image 
 (NM_image) = image_filter(NM_lon_rotated, NM_lat_rotated)
 # Produces the heatmap 
 (NM_aurora, NM_flux) = image_heatmap(idl_files[0], NM_lon_rotated, NM_lat_rotated, NM_image)
+
 
 # Method 1 (Comment out if Method 2 is being implemented, also look inside the loop for further instructions)
 
@@ -132,25 +152,34 @@ Results = np.zeros([nSats,nTimes])
 
 iSat = 0 
 
+
 while iSat < nSats: # While there are satellites in orbit 
 
     iTime = 0
 
     while iTime < nTimes:	# While there are pictures to be taken 
 
+        iFile = int(np.round(nFiles * float(iTime / nTimes)))
+        
         # Satellite's x-displacement per picture taken 
         x = OrbitX[iSat][iTime] + 1  
         # Satellite's y-displacement per picture taken
         y = 1
         # Satellite's z-displacement per picture taken
         z = OrbitZ[iSat][iTime] + 1
-        
+
+        vPole = make_vector(-x, -y, np.sign(z) * rEarth - z)
+        vCenter = make_vector(-x, -y, -z)
+        angle = get_angle(vPole, vCenter)
+        shift = 0.0 # np.sign(z) * angle / alpha / np.cos(angle)
+
+        print('angle to pole : ', angle, shift)
+            
         # Get image: 
-        
-        (NM_lon, NM_lat) = image_grid(x,y,z,n,m,alpha,rEarth)
+        (NM_lon, NM_lat) = image_grid(x, y, z, n, m, alpha, rEarth, shift)
         (NM_lon_rotated, NM_lat_rotated) = image_rotation(NM_lon, NM_lat, x, y, z, rEarth)
         (NM_image) = image_filter(NM_lon_rotated, NM_lat_rotated)
-        (NM_aurora, NM_flux) = image_heatmap(idl_files[0], NM_lon_rotated, NM_lat_rotated, NM_image)
+        (NM_aurora, NM_flux) = image_heatmap(idl_files[iFile], NM_lon_rotated, NM_lat_rotated, NM_image)
         
         # Method 1: (Comment out if Method 2 is being implemented, also look outside of the loop for further instructions)
         
@@ -179,8 +208,8 @@ while iSat < nSats: # While there are satellites in orbit
         # Making sure the user sees that the program is running without any errors: 
         
         print('')
-        print('Current Satellite: ' + str(iSat + 1))
-        print('Current Frame: ' + str(iTime + 1))
+        print('Current Satellite: ' + str(iSat + 1) + ' of ' + str(nSats))
+        print('Current Frame: ' + str(iTime + 1) + ' of ' + str(nTimes))
         print(NM_result)
         	
         iTime += 1 # Iterator goes up 
